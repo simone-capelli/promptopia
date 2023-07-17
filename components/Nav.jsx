baa7e2d67cfd830d2105a9510baaaa7a829a6d2f
simo-sw <simonecapelli13@gmail.com>
2023-07-17 10:32:02 +0200

    Do not pull - Homepage implemented but providers are null

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,26 +6,23 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
-      const response = await getProviders();
-
-      setProviders(response);
-    };
-
-    setProviders();
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
   }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src="/assets/images/logo.svg"
+          src={session?.user.image}
           alt="Promptopia Logo"
           width={30}
           height={30}
@@ -34,9 +31,11 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
+      {alert(providers)}
+
       {/* Mobile Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -48,7 +47,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -76,7 +75,7 @@ const Nav = () => {
       </div>
 
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
---
+++ utils/database.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDb = async () => {
+  mongoose.set('strictQuery', true);
+
+  if (isConnected) {
+    alert('connected');
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    alert('connecting');
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'share_prompt',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+
+    console.log('MongoDb connected');
+  } catch (error) {
+    alert('NOT connected');
+    console.log(error);
+  }
+};
