24e309f0025e5ff1d7cc08abde61ed45cff9afc4
simo-sw <simonecapelli13@gmail.com>
2023-07-30 16:52:56 +0200

    View other profiles implemented! :sparkles:

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -12,6 +12,11 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
   const [copied, setCopied] = useState('');
 
+  const handleProfileClick = () => {
+    if (session?.user._id === post.creator.id) return router.push('/profile');
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
@@ -21,7 +26,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
